9b9141ada2560966ed7991841a6310347443b304
moosch <mail@moosch.io>
2023-12-18 21:05:32 +1030

    Lexer updated

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -32,7 +32,14 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+        if l.peekChar() == '=' {
+            ch := l.ch
+            l.readChar()
+            literal := string(ch) + string(l.ch)
+            tok = token.Token{Type: token.EQ, Literal: literal}
+        } else {
+    		tok = newToken(token.ASSIGN, l.ch)
+        }
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
 	case ')':
@@ -48,7 +55,14 @@ func (l *Lexer) NextToken() token.Token {
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+        if l.peekChar() == '=' {
+            ch := l.ch
+            l.readChar()
+            literal := string(ch) + string(l.ch)
+            tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+        } else {
+    		tok = newToken(token.BANG, l.ch)
+        }
 	case '*':
 		tok = newToken(token.ASTERISK, l.ch)
 	case '<':
@@ -96,6 +110,14 @@ func (l *Lexer) readIdentifier() string {
         l.readChar()
     }
     return l.input[position:l.position]
+}
+
+func (l *Lexer) peekChar() byte {
+    if l.readPosition >= len(l.input) {
+        return 0;
+    } else {
+        return l.input[l.readPosition]
+    }
 }
 
 func isLetter(ch byte) bool {
